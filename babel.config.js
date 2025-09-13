8f5410acef66a0bd89e75f2ca5e26098bd4537c6
henry <aengrhenry@gmail.com>
2025-09-13 17:32:12 +0100

    configuration for eas build

--- babel.config.js
+++ babel.config.js
@@ -5,6 +5,17 @@
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
     ],
+    plugins: [
+      [
+        'module:react-native-dotenv',
+        {
+          moduleName: '@env',
+          path: '.env',
+          safe: false,
+          allowUndefined: true,
+        },
+      ],
+    ],
   };
 };
  
